a2aa8b917a014677ffb067dbbf10c093cd9de009
kai-blt <blaitong@gmail.com>
2020-12-16 14:22:55 -0800

    added more controller tests

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTestUnitNoDB.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerTestUnitNoDB.java
@@ -17,12 +17,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -30,6 +32,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -223,11 +229,70 @@ public class UserControllerTestUnitNoDB {
     }
 
     @Test
-    public void getUserLikeName() {
+    public void getUserLikeName() throws Exception{
+        //Mock API Url
+        String apiUrl = "/users/user/name/like/admin";
+
+        //Mock the service call to find user by id
+        Mockito.when(userService.findByNameContaining("admin")).thenReturn(userList);
+
+        //Make a request builder to mock client request
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+
+        //Perform the request and return the result
+        MvcResult result = mockMvc.perform(rb).andReturn();
+
+        //Get the response from the result and convert to string
+        String testResult = result.getResponse().getContentAsString();
+
+        //Convert Array from Java to JSON Object Mapper
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedResult = mapper.writeValueAsString(userList);
+
+        assertEquals(expectedResult, testResult);
+
     }
 
     @Test
-    public void addNewUser() {
+    public void addNewUser() throws Exception {
+        //Mock API Url
+        String apiUrl = "/users/user";
+
+        //Mock up a user
+        User u1 = new User("Testadmin3",
+                "password",
+                "admin@lambdaschool.local");
+        u1.setUserid(30);
+
+        //Mock their role
+        Role r1 = new Role("admin");
+        r1.setRoleid(1);
+
+        u1.getRoles()
+                .add(new UserRoles(u1, r1));
+
+        //Mock their email
+        u1.getUseremails()
+                .add(new Useremail(u1,"admin@email.local"));
+        u1.getUseremails().get(0).setUseremailid(11);
+
+        //Convert from Java to JSON Object Mapper
+        ObjectMapper mapper = new ObjectMapper();
+        String userString = mapper.writeValueAsString(u1);
+
+        //When save method called from service, provide return our user
+        Mockito.when(userService.save(any(User.class)))
+                .thenReturn(u1);
+
+        //Mock the post request
+        RequestBuilder rb = MockMvcRequestBuilders.post(apiUrl)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userString);
+
+        mockMvc.perform(rb)
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
@@ -239,6 +304,16 @@ public class UserControllerTestUnitNoDB {
     }
 
     @Test
-    public void deleteUserById() {
+    public void deleteUserById() throws Exception {
+        String apiUrl = "/users/user/{userid}";
+
+        RequestBuilder rb = MockMvcRequestBuilders.delete(apiUrl,
+                "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(rb)
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 }
